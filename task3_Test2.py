af19df4b5e1bcb67393d627535379b5a3557c558
Ethan Leavold <ethanleavold@gmail.com>
2025-06-18 17:19:52 +0100

    Committing updated files for issue #1 and issue #2

--- task3_Test2.py
+++ task3_Test2.py
@@ -1,3 +1,5 @@
+# Collects user input
 user_input = input("Input Text: ")
 
+# Prints user input
 print(user_input)
---
+++ task3_Test3.py
@@ -0,0 +1,6 @@
+# Collects user input
+# Capitalizes the user's input
+user_input = input("Input Text: ").capitalize()
+
+# Prints user's capitalized input
+print(user_input)
